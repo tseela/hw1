7a2d4fa27fc6aef8238b845d5a2ad973b7208711
tseela <tseelagamzo@gmail.com>
2020-08-13 16:01:31 +0300

    I didn't change anything but it says I need to make a commit

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tseela/hw1/hw1
-# Build directory: /home/tseela/hw1/hw1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
