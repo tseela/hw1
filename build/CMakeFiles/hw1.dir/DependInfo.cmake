05d6d359dae301ac5dcf4a4169b1005f6324478d
tseela <tseelagamzo@gmail.com>
2020-08-13 15:40:22 +0300

    added gitignore, cmake and details to branch dev too

---
+++ build/CMakeFiles/hw1.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/tseela/hw1/hw1/ErrorCode.c" "/home/tseela/hw1/hw1/build/CMakeFiles/hw1.dir/ErrorCode.c.o"
+  "/home/tseela/hw1/hw1/Matrix.c" "/home/tseela/hw1/hw1/build/CMakeFiles/hw1.dir/Matrix.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "hw1_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/hw1.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "libhw1.a"
+)
